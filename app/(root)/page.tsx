83721525d9f8db73ab36a846cf3d5d2815c4cd0a
cnarunner <brycejensen97@hotmail.com>
2025-02-19 14:15:03 -0700

    Post Cards created
    https://youtu.be/Zq5fmkH0T78?si=wu_vlSYojNkpa4ya&t=7958

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,32 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
 
 export default async function Home({searchParams}: {
     searchParams: Promise<{ query?: string}>
 }) {
     const query = (await searchParams).query;
+
+    const posts  = [
+        {
+            _createdAt: new Date(),
+            views: 55,
+            author: {_id: 1, name: 'Creative Genius'},
+            _id: 1,
+            title: 'Muse 1',
+            description: 'Muse 1 description',
+            image: 'https://picsum.photos/600/900',
+            category: 'Muse Test'
+        },
+        {
+            _createdAt: new Date(),
+            views: 1000,
+            author: {_id: 2, name: 'Pink Peel Creations'},
+            _id: 2,
+            title: 'Muse 2',
+            description: 'Muse 2 description',
+            image: 'https://picsum.photos/300/450',
+            category: 'Muse Test'
+        }];
 
   return (
     <>
@@ -18,6 +41,22 @@ export default async function Home({searchParams}: {
 
             <SearchForm query={query}/>
 
+        </section>
+
+        <section className={"section_container"}>
+            <p className={"text-30-semibold"}>
+                {query ? `Search results for "${query}"` : 'All posts' }
+            </p>
+
+            <ul className={"mt-7 card_grid"}>
+                {posts?.length > 0 ? (
+                    posts.map((post: StartupTypeCard) => (
+                        <StartupCard key={post?._id} post={post} />
+                    ))
+                ) : (
+                    <p className="no-results">No startups found</p>
+                )}
+            </ul>
         </section>
     </>
   );
