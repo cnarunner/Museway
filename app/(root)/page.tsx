6c269e5d103dd8a4519353d7bd103aea37ddc1b9
cnarunner <brycejensen97@hotmail.com>
2025-02-18 23:04:46 -0700

    Search Form implemented

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,23 @@
 import Image from "next/image";
 import SearchForm from "@/app/components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}: {
+    searchParams: Promise<{ query?: string}>
+}) {
+    const query = (await searchParams).query;
+
   return (
     <>
         <section className={"pink_container"}>
-            <h1 className={"heading"}>Post your muse! <br/> We would love to see it!</h1>
+            <h1 className={"heading"}>
+                Post your muse! <br/> We would love to see it!
+            </h1>
 
             <p className={"sub-heading !max-w-3xl"}>
                 Share your creative process for the world to see!
             </p>
 
-            <SearchForm/>
+            <SearchForm query={query}/>
 
         </section>
     </>
--- app/components/SearchForm.tsx
+++ app/components/SearchForm.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Form from "next/form";
+import {reset} from "next/dist/lib/picocolors";
+import SearchFormReset from "@/app/components/SearchFormReset";
 
-const SearchForm = () => {
+const SearchForm = ({query}: {query?: string}) => {
+
     return (
-        <div>SearchForm</div>
+        <Form action={"/"} scroll={false} className={"search-form"}>
+            <input
+                name={"query"}
+                defaultValue={query}
+                className={"search-input"}
+                placeholder={"Search Posts"}
+            />
+
+            <div className={"flex gap-2"}>
+                {query &&  <SearchFormReset/> }
+
+                <button type={"submit"} className={"search-btn text-white"}>
+                    S
+                </button>
+            </div>
+        </Form>
     )
 }
 export default SearchForm
