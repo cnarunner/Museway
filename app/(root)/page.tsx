315e976b2a060b79749b0ed7ac94888a323dd2d5
Bryce Jensen <brycejensen97@gmail.com>
2025-02-19 23:02:37 -0700

    sanity is set up, all posts are displayed on Home
    https://youtu.be/Zq5fmkH0T78?si=LwpR1Z2ScrrvU05C&t=9528

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,32 +1,38 @@
 import SearchForm from "@/components/SearchForm";
 import StartupCard from "@/components/StartupCard";
+import {client} from "@/sanity/lib/client";
+import {POST_QUERY} from "@/sanity/lib/queries";
 
 export default async function Home({searchParams}: {
     searchParams: Promise<{ query?: string}>
 }) {
     const query = (await searchParams).query;
 
-    const posts  = [
-        {
-            _createdAt: new Date(),
-            views: 55,
-            author: {_id: 1, name: 'Creative Genius'},
-            _id: 1,
-            title: 'Muse 1',
-            description: 'Muse 1 description',
-            image: 'https://picsum.photos/300/450',
-            category: 'Muse Test'
-        },
-        {
-            _createdAt: new Date(),
-            views: 1000,
-            author: {_id: 2, name: 'Pink Peel Creations'},
-            _id: 2,
-            title: 'Muse 2',
-            description: 'Muse 2 description',
-            image: 'https://picsum.photos/300/450',
-            category: 'Muse Test'
-        }];
+    const posts = await client.fetch(POST_QUERY)
+
+    console.log(JSON.stringify(posts, null, 2))
+
+    // const posts  = [
+    //     {
+    //         _createdAt: new Date(),
+    //         views: 55,
+    //         author: {_id: 1, name: 'Creative Genius'},
+    //         _id: 1,
+    //         title: 'Muse 1',
+    //         description: 'Muse 1 description',
+    //         image: 'https://picsum.photos/300/450',
+    //         category: 'Muse Test'
+    //     },
+    //     {
+    //         _createdAt: new Date(),
+    //         views: 1000,
+    //         author: {_id: 2, name: 'Pink Peel Creations'},
+    //         _id: 2,
+    //         title: 'Muse 2',
+    //         description: 'Muse 2 description',
+    //         image: 'https://picsum.photos/300/450',
+    //         category: 'Muse Test'
+    //     }];
 
   return (
     <>
